759c54638ea3ebd5cf77a7863b8628d099a556c1
ganeshjogi <ganeshjogi2000@gmail.com>
2021-07-18 11:05:58 +0530

    database fixed

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app = require("./app");
 // mongo db connection
 // User name and password is given here as open due to access for but this type of practice is not good use env varibales and set them in production website
 mongoose
-  .connect(
-    "mongodb+srv://ganesh:yOaSYTtvMyh7HNPx@cluster0.npnmu.mongodb.net/natours?retryWrites=true&w=majority"
-  )
+  .connect()
   .then((con) => {
     console.log("DB connection established");
   });
